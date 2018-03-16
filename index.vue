879c4d0e44e2ac89d68d890ce7039d0d27fc5f2e
Abhishek chopra <abhishek@haltdos.com>
2018-03-16 14:47:57 +0530

    FlexComplete

---
+++ index.vue
@@ -0,0 +1,75 @@
+
+<template>
+
+<div class="page container">
+    <div class="title">Welcome to {{Header}}</div>
+    <div class="subtitle">How are you ?</div>
+    <div class="projectList">
+        <!-- eslint-disable-next-line -->
+        <project v-for="project in projAry"
+            :key="project.title"
+            :title="project.title"
+            :desc="project.desc"
+        ></project>
+    </div>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</div>
+
+</template>
+
+<style>
+    body{
+        background-image: url('./bg.jpg');
+        background-size: cover;
+    }
+    .title{
+        text-align: center;
+        font-weight: bold;
+        font-size: xx-large;
+        margin:1.5vmax 0;
+        color:#f0ffff;
+    }
+    .projectList{
+        display:flex;
+        justify-content: center space-around;
+        flex-wrap: wrap;
+    }
+</style>
+
+<script type="ts">
+import Vue from 'vue'
+import project from "./project"
+let projAry = [
+    {title:"Proj#1",
+     desc:"lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+    },
+    {title:"Proj#2",
+     desc:"lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+    },
+    {title:"Proj#3",
+     desc:"lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+    },
+    {title:"Proj#4",
+     desc:"lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+    },
+
+    {title:"Proj#5",
+     desc:"lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+    },
+    {title:"Proj#6",
+     desc:"lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+    },
+    {title:"Proj#8",
+     desc:"lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+    },
+]
+export default Vue.extend({
+    components:{
+        project: project
+    },
+    data:()=>({
+        Header: "Apps.achopra.me",
+        projAry:projAry
+    })
+})
+</script>
---
+++ project.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="project card" href="www.google.com">
+        <div class="card-header">
+            {{title}}
+        </div>
+        <div class="card-body project-description">
+            {{desc}}
+        </div>
+        <a class="projBtn btn btn-primary">Visit</a>
+    </div>
+</template>
+
+<style>
+.projBtn{
+    margin: 2vmax auto;
+    max-width: 75%;
+    min-width: 50%;
+}
+.project{
+    margin:1vmax 1vmax;
+}
+.project:hover {
+    transform: translate(10%,10%,0);
+}
+@media (min-width: 600px) {
+    .project{
+        width:95%;
+    }
+}
+@media (min-width: 768px){
+    .project{
+        width: 45%;
+        /* margin:1vmax auto; */
+    }
+}
+@media (min-width: 992px){
+    .projBtn{
+        margin: 0.5vmax auto;
+    }
+    .project{
+        width: 25%;
+        /* margin:1vmax 1vmax; */
+    }
+}
+</style>
+
+<script type="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props:['title','desc']
+})
+</script>
