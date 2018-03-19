4d0f772ed626536fe0899632da8561f998afc403
Abhishek chopra <abhishek@haltdos.com>
2018-03-19 13:27:25 +0530

    fixed?

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mv -fT dist/ docs/
 #Replace absolute path with relative for hosting
 sed -i 's/\/js/js/g' docs/index.html
 sed -i 's/\/css/css/g' docs/index.html
-sed -i 's/\/img/img/g' docs/css/*.css
+sed -i 's/\/img/..\/img/g' docs/css/*.css
 #Change title of the App
 sed -i "s/Vue CLI App/AC\'s Apps/g" docs/index.html
 git add -A
