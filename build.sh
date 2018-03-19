b85f067f9410e9fa5274873b644788e75feac55b
Abhishek chopra <abhishek@haltdos.com>
2018-03-19 13:13:50 +0530

    added build directives for img

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ rm -rf docs
 mv -fT dist/ docs/
 sed -i 's/\/js/js/g' docs/index.html
 sed -i 's/\/css/css/g' docs/index.html
+sed -i 's/\/img/img/g' docs/index.html
 sed -i "s/Vue CLI App/AC\'s Apps/g" docs/index.html
 git add -A
 exit
